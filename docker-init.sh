ebe4164f012a4e1093b042a00d01873539c037e3
Matias-Antilef <matias.romero.dv@gmail.com>
2025-05-08 10:13:37 -0400

    final

--- docker-init.sh
+++
@@ -1,8 +0,0 @@
-echo "Instalando dependencias (npm install)"
-npm install
-
-echo "Inicializando cliente de prisma"
-npx prisma migrate dev --name "init"
-
-echo "Inicializando PostgreSQL (docker-compose up)"
-docker-compose up
