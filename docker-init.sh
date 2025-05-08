39ac43e8e8989f696a4cb226c27f5a1106db3ce8
Matias-Antilef <matias.romero.dv@gmail.com>
2025-05-08 09:25:45 -0400

    Reutilización de variables

--- docker-init.sh
+++ docker-init.sh
@@ -1,11 +1,8 @@
 echo "Instalando dependencias (npm install)"
 npm install
 
-echo "Inicializando PostgreSQL (docker-compose up)"
-docker-compose up
-
 echo "Inicializando cliente de prisma"
 npx prisma generate --name "init"
 
-echo "Inicializando backend"
-npm run dev 
+echo "Inicializando PostgreSQL (docker-compose up)"
+docker-compose up
